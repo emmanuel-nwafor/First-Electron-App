50078a7ddd6459d9da588232161bd3df25fe28a8
emmanuel-nwafor <emitexc.e.o1@gmail.com>
2026-04-01 16:02:54 +0100

    restructring the app layout

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      // It's safer to keep these settings for modern Electron apps
       contextIsolation: true,
       nodeIntegration: false
     }
@@ -15,23 +14,16 @@ function createWindow() {
 
   // Load your index.html
   mainWindow.loadFile('index.html');
-
-  // OPTIONAL: Open DevTools automatically to help you learn
-  // mainWindow.webContents.openDevTools();
 }
 
-// This method will be called when Electron has finished initialization
 app.whenReady().then(() => {
   createWindow();
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-// Quit when all windows are closed, except on macOS.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
 });
